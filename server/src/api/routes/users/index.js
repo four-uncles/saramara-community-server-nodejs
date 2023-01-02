9bc739608beae46a8d6713c3995e0f50e372758d
hb9397 <hb9397@naver.com>
2023-01-02 18:34:01 +0900

    refactor : routes users.js Delete and create users directoty 20230102 이현범

---
+++ server/src/api/routes/users/index.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import join from './join.js'
+
+/* 
+ * users 디렉토리의 API들을 모두 내보낸다.
+ * join, login, refresh, ...
+ */
+export default (app) => { // Router() 함수를 매개변수로 받는다
+	// Router() 함수를 각각의 실제 라우터로 등록하는 로직이 있는 라우터 파일로 넘겨줘서 등록한다.
+	
+    join(app); 
+	
+
+	return app
+}
--- server/src/api/routes/users/join.js
+++ server/src/api/routes/users/join.js
@@ -1,18 +1,21 @@
 /* 
- * /users/ 와 관련된 routes 
+ * api/join router
  */
 import { Router } from "express";
-import Users from "../../models/users.js";
-import Logger from "../../loaders/logger.js"
+import Users from "../../../models/users.js";
+import Logger from "../../../loaders/logger.js"
 import bcrypt from "bcrypt";
 
 const router = Router();
 
-const users = (app) => {
-    app.use(router);
+const join = (app) => {
+    // 매개변수로 받은 최상위 routes에 index.js를 라우터로 사용하기 위한 Router() 함수를 app으로 받아 
+    // 현재 파일을 Router()를 이용해서 라우터로 만들고 최상위 index로 부터 받은 Router() = app 함수를 사용해 현재 파일을 라우터로 등록
+    app.use('/register',router);
+
     // 회원가입 처리 api - middleware 디렉토리 이동 의심 발생
     // TODO : 가입된 회원인지 검증하는 로직 미처리
-    router.post("/register/join", async(req, res, next) => {
+    router.post("/join", async(req, res, next) => {
         const {email, password, nickname} = req.body;
         
         // 비밀번호 해싱 처리화
@@ -48,4 +51,4 @@ const users = (app) => {
     
 };
 
-export default users;
+export default join;

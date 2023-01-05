f62dd6abbd6ee0f6138f98419622c2b81563d5f6
hb9397@naver.com <hb9397@naver.com>
2023-01-05 19:34:02 +0900

    fix refresh 라우터 수정 및 등록, refreshVerify 메서드, longin.js, jwtService 주석 추가

--- server/src/api/routes/users/index.js
+++ server/src/api/routes/users/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import register from './register.js'
 import login from './login.js'
+import refresh from './refresh.js';
 
 /* 
  * users 디렉토리의 API들을 모두 내보낸다.
@@ -9,6 +10,7 @@ import login from './login.js'
 export default (app) => { // Router() 함수를 매개변수로 받는다
 	// Router() 함수를 각각의 실제 라우터로 등록하는 로직이 있는 라우터 파일로 넘겨줘서 등록한다.
 	
+	refresh(app);
     register(app); 
 	login(app);
 
--- server/src/services/jwtService.js
+++ server/src/services/jwtService.js
@@ -1,16 +1,18 @@
 import jwt from 'jsonwebtoken'
 import config from '../config/config.js'
+import Tokens from '../models/tokens.js'
 
 const secret = config.jwtSecret;
 
 const jwtService = {
+  // 입력 받은 userinfo 중 email과 role 로 payload 생성
   sign: (userinfo) => {
-    console.log(secret);
     const payload = {
       email: userinfo.email,
       role: userinfo.role,
     };
 
+    // payload, 해독키, 해싱 알고리즘, 만료기간, 발급자로 만든 jwt 반환
     return jwt.sign(payload, secret, {
       algorithm: config.jwtAlgorithm,
       expiresIn: config.jwtAccessTokenExpiresIn,
@@ -36,17 +38,25 @@ const jwtService = {
   },
 
   refresh: () => {
+    // sing 함수에게 반환 받은  jwt에 다시 jwt 암호화 키와 알고리즘, 만료기간, 발급자 를 암호화해서 refrsesh token 생성해서 반환
     return jwt.sign({}, secret, {
-      algorithm: 'HS256',
-      expiresIn: '14d',
-      issuer: 'kshired',
+      algorithm: config.jwtAlgorithm,
+      expiresIn: config.jwtAccessTokenExpiresIn,
+      issuer: config.jwtIssuer,
     });
   },
   
-  refreshVerify: async (token, username) => {
-    const getAsync = promisify(redisClient.get).bind(redisClient);
+  refreshVerify: async (token, email) => {
+    const getRefreshToken = await Tokens.findOne({
+      where: {
+        email: email
+      }
+    });
+    console.log(email);
+    console.log(getRefreshToken);
+    // const getAsync = promisify(redisClient.get).bind(redisClient);
     try {
-      const data = await getAsync(username);
+      const data = await getRefreshToken.refreshToken;
       if (token === data) {
         return {
           ok: true,
